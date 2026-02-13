5533a35a1619ef8efaad44eecf6da61ed035cda0
cbhatia511-dot <cbhatia511@gmail.com>
2026-02-13 23:49:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+  document.getElementById("message").classList.remove("hidden");
+}
+
+function yes() {
+  document.getElementById("finalMessage").innerHTML =
+    "Yayyy 😍💖 I knew it! I LOVE YOU SO MUCH ❤️🔥";
+}
+
+function no() {
+  document.getElementById("finalMessage").innerHTML =
+    "No option available 😏💘 You are mine forever 😘❤️";
+}
